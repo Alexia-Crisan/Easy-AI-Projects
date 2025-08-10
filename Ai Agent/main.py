f089bea88eaf124cec095b430373fba22469e976
Alexia-Crisan <150911612+Alexia-Crisan@users.noreply.github.com>
2025-08-10 19:49:51 +0300

    agent working

--- Ai Agent/main.py
+++ Ai Agent/main.py
@@ -3,13 +3,26 @@ from langchain_openai import ChatOpenAI #allows the usage of openAi within langc
 from langchain.tools import tool
 from langgraph.prebuilt import create_react_agent # high level framework that allows to build AI agent
 from dotenv import load_dotenv
+import os
+from pathlib import Path
 
-load_dotenv() #loads stuff from .env
+load_dotenv()
+
+@tool
+def calculator(a: float, b: float) -> str:
+    """Useful for performing basic arithmetic calculations with numbers"""
+    return f"The sum of {a} and {b} is {a + b}"
+
+@tool
+def say_hello(name: str) -> str:
+    """Useful for greeting a user"""
+    return f"Hi {name} my brooooo"
 
 def main():
-    model = ChatOpenAI(temperature = 0) #the higher the temperature, the more randomness
+    api_key = ""
+    model = ChatOpenAI(temperature=0, openai_api_key=api_key) #the higher the temperature, the more randomness
 
-    tools = []
+    tools = [calculator, say_hello]
     agent = create_react_agent(model, tools)
 
     print("Welcome! I'm your AI assistent. Type 'quit to exit.'")
@@ -21,7 +34,7 @@ def main():
         if user_input == "quit":
             break
 
-        print("\nAI Assistant ", end = "")
+        print("\nAI Assistant: ", end = "")
 
         for chunk in agent.stream({"messages" : [HumanMessage(content = user_input)]}):
             if "agent" in chunk and "messages" in chunk["agent"]:
